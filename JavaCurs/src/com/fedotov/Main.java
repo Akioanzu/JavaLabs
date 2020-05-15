582539db6b039a7597782e33dcc40c62f0b94d43
Akioanzu <56451475+Akioanzu@users.noreply.github.com>
2020-05-15 22:53:30 +0300

    Release

--- JavaCurs/src/com/fedotov/Main.java
+++ JavaCurs/src/com/fedotov/Main.java
@@ -1,28 +1,32 @@
 package com.fedotov;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         boolean cntne = true;
         while (cntne) {
-        Scanner in = new Scanner(System.in);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("1 - добавить статью" + "\n");
         System.out.print("2 - поиск по ключевому слову"+ "\n");
         System.out.print("3 - завершить программу"+ "\n");
         System.out.print("Введите действие: ");
-        int v = in.nextInt();
-        switch (v) {
+        int v = Integer.parseInt(reader.readLine());
+       switch (v) {
             case 1:
                 try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/CursJava", "postgres", "postgres")) {
                     Statement statement = connection.createStatement();
                     System.out.print("Введите ключевое слово: ");
-                    String keyword = in.next();
-                    System.out.print("Введите Описание: ");
-                    String discription = in.next();
+                    String keyword = reader.readLine();
+                    System.out.print("Введите Описание: "+ "\n");
+                    String discription = reader.readLine();
                     int result = statement.executeUpdate("insert into public.tbl_dictionary (keyword, discription) VALUES (" + "'" + keyword + "'" + "," + "'" + discription + "'" + ")");
                     System.out.print("Статья добавлена");
-                } catch (SQLException e) {
+                    System.out.print("\n");
+                } catch (SQLException | IOException e) {
                     System.out.println("Connection failure.");
                     e.printStackTrace();
                 }
@@ -31,12 +35,13 @@ public class Main {
                 try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/CursJava", "postgres", "postgres")) {
                     Statement statement = connection.createStatement();
                     System.out.print("Введите ключевое слово: ");
-                    String key = in.next();
+                    String key = reader.readLine();
                     System.out.printf("%-30.30s  %-30.30s%n", "Ключевое слово", "Описание");
                     ResultSet resultSet = statement.executeQuery("SELECT keyword, discription FROM public.tbl_dictionary where tbl_dictionary.keyword =" + "'" + key + "'");
                     while (resultSet.next()) {
                         System.out.printf("%-30.150s  %-30.150s%n", resultSet.getString("keyword"), resultSet.getString("discription"));
-                    }
+                   }
+                    System.out.print("\n");
                 } catch (SQLException e) {
                     System.out.println("Connection failure.");
                     e.printStackTrace();
